4f47faddf63a8285cd5de73b93a6e51970e244b4
creative-style <clodoaldo.neto.116@ufrn.edu.br>
2021-10-01 09:37:39 -0300

    add congif bd

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,13 +5,15 @@ import cors from 'cors';
 // import { errors } from 'celebrate';
 import routes from './../routes';
 import AppError from '@shared/errors/AppError';
-// import '@shared/typeorm';
+import '@shared/typeorm';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+/**
+ * @description Depois da criação de rotas fazemos esse tratamento com o middleware de eeros customizados*/
 app.use(
     (
         error: Error,
@@ -19,8 +21,9 @@ app.use(
         response: Response,
         next: NextFunction,
     ) => {
+        /**
+         * @description se o erro for uma instancia da nossa classe, usamos a classe para tratar*/
         if (error instanceof AppError) {
-            //se o erro for uma instancia usamos a classe para tratar
             return response.status(error.statusCode).json({
                 status: 'error',
                 message: error.message,
