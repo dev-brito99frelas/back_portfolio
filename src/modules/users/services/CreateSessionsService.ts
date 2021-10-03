060826dea323bd397843ed1af548445177bc1537
creative-style <clodoaldo.neto.116@ufrn.edu.br>
2021-10-03 19:47:38 -0300

    create session and token

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -3,16 +3,18 @@ import { compare } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
+import { sign } from 'jsonwebtoken';
 
 interface IRequest {
     email: string;
     password: string;
 }
-// interface IResponse {
-//     user: User;
-// }
+interface IResponse {
+    user: User;
+    token: string;
+}
 class CreateSessionsService {
-    public async execute({ email, password }: IRequest): Promise<User> {
+    public async execute({ email, password }: IRequest): Promise<IResponse> {
         const usersRepository = getCustomRepository(UsersRepository);
         const user = await usersRepository.findByEmail(email);
         if (!user) {
@@ -23,7 +25,14 @@ class CreateSessionsService {
         if (!passwordConfirmed) {
             throw new AppError('Incorrect emal/password combination', 401);
         }
-        return user;
+        const token = sign({}, 'dts5684647svdfgs&sdva#fDReS#@!b2', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+        return {
+            user,
+            token,
+        };
     }
 }
 export default CreateSessionsService;
--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,8 +1,10 @@
 import postsRouter from '@modules/posts/routes/Posts.routes';
 import usersRouter from '@modules/users/routes/Users.routes';
+import sessionsRouter from '@modules/users/routes/Sessions.routes';
 import { Router } from 'express';
 
 const routes = Router();
 routes.use('/posts', postsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 export default routes;
