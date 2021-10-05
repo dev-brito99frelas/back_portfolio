4f5390b8c2b6c41fd7e2eba11571bf8d12d38e40
creative-style <clodoaldo.neto.116@ufrn.edu.br>
2021-10-05 09:47:07 -0300

    added profile routes

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -3,10 +3,12 @@ import usersRouter from '@modules/users/routes/Users.routes';
 import sessionsRouter from '@modules/users/routes/Sessions.routes';
 import { Router } from 'express';
 import passwordRouter from '@modules/users/routes/Password.routes';
+import profileRouter from '@modules/users/routes/Profile.routes';
 
 const routes = Router();
 routes.use('/posts', postsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 export default routes;
